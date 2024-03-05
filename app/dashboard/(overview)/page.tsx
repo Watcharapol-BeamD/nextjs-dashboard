b2875c5aa7f6997a16424e73380c20a071db3876
Watcharapol-BeamD <watcharapol.numpaya@gtnexttech.com>
2024-03-05 17:53:20 +0700

    [HAFT] Chapter 9 (Overview)

--- app/dashboard/(overview)/page.tsx
+++ app/dashboard/(overview)/page.tsx
@@ -2,7 +2,7 @@ import { Card } from '@/app/ui/dashboard/cards';
 import RevenueChart from '@/app/ui/dashboard/revenue-chart';
 import LatestInvoices from '@/app/ui/dashboard/latest-invoices';
 import { lusitana } from '@/app/ui/fonts';
-import async from './../ui/invoices/table';
+import async from '../../ui/invoices/table';
 import { Revenue } from '@/app/lib/definitions';
 import { fetchRevenue, fetchLatestInvoices,fetchCardData } from '@/app/lib/data';
 
